255232a202616c5955253934fe24772b94742149
Irina <demi.irina.96@gmail.com>
2021-04-10 19:53:50 +0300

    finally works

--- subpub.py
+++ subpub.py
@@ -1,35 +1,37 @@
 import paho.mqtt.client as mqtt
 import threading
+import json
+import random
 
-a = None
+c = None
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe('topic')
 
 def on_message(client, userdata, msg):
-    global a
-    if msg.payload.decode() != a:
-        print(msg.payload.decode())
+    global c
+    if msg.payload.decode() != c:
+        print(json.loads(msg.payload.decode()))
     else:
-        a = None
+        c = None
 
-client = mqtt.Client()
-
-client.connect('172.17.0.1', 1883, 60)
-
-def mes():
-    client.on_connect = on_connect
-    client.on_message = on_message
-
+def loop():
     client.loop_forever()
 
-t1 = threading.Thread(target = mes)
-t1.start()
+client = mqtt.Client()
+client.connect('172.17.0.1', 1883, 60)
+client.on_connect = on_connect
+client.on_message = on_message
 
-name = input('enter your name: ')
+name = input('Enter your name: ')
+a = '\033[' + str(random.randint(31, 36)) + 'm'
+color = a + name + ': ' + '\033[0m'
+
+t1 = threading.Thread(target = loop)
+t1.start()
 
 while True:
     b = input()
-    a = name + ': ' + b
-    client.publish('topic', a)
+    c = json.dumps(color + b)
+    client.publish('topic', c)
 
